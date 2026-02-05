8acdeceb3a47b4493a9b446907f37a71ef04eefe
Vishwa Kaushalya <165917431+VKaushalya03@users.noreply.github.com>
2026-02-05 18:23:38 +0530

    host

--- server/server.js
+++ server/server.js
@@ -13,6 +13,9 @@ app.use(express.json());
 app.use(
   cors({
     origin: ["https://mood-music-steel.vercel.app", "http://localhost:5173"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204,
   }),
 );
 
