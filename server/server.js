ec94587cfac537c9c3d478814634bd3a367c4e30
Vishwa Kaushalya <165917431+VKaushalya03@users.noreply.github.com>
2026-02-05 17:35:06 +0530

    host

--- server/server.js
+++ server/server.js
@@ -10,7 +10,11 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://your-project-name.vercel.app", "http://localhost:5173"],
+  }),
+);
 
 // Define Routes
 app.use("/api/auth", require("./routes/auth"));
